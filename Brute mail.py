b6401528f219b835dc234219c7cd63d594c611b6
Morwenns <61369008+Morwenns@users.noreply.github.com>
2020-03-18 19:40:19 +0800

    Update Brute mail.py

--- Brute mail.py
+++ Brute mail.py
@@ -3,7 +3,7 @@ import email.utils
 s = "n" 
 while s in ['n','N']:
 	print("""\033[1;32m
-	 _____         _                         _ _  | Author: Joynses
+	 _____         _                         _ _  | Author: 
 	| __  |___ _ _| |_ ___ ___ ___ _____ ___|_| | | Date: 14/03/2020
 	| __ -|  _| | |  _| -_|___| -_|     | .'| | | | Tool: Brute-force for Gmail
 	|_____|_| |___|_| |___|   |___|_|_|_|__,|_|_| | More info : Easy-Brute
